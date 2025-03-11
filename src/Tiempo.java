c953cec599d8780c27f53ae0f3750d22440c17fc
Paula <agostinellipaula7@gmail.com>
2025-03-11 18:46:07 -0300

    Forma anemica

--- src/Tiempo.java
+++ src/Tiempo.java
@@ -1,2 +1,18 @@
+import java.time.LocalDate;
+
 public class Tiempo {
+    private LocalDate fecha;
+
+    public Tiempo() {
+        this.fecha = LocalDate.now();
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
 }
+
